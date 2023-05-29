b93a11a75d12f5cee55ebbf897e6d4fec159804a
Svetlio <shapeshifter003@gmail.com>
2023-05-29 13:51:24 +0200

    caching

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-import { BehaviorSubject, catchError, combineLatest, filter, map, merge, Observable, scan, Subject, tap, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, combineLatest, filter, map, merge, Observable, scan, shareReplay, Subject, tap, throwError } from 'rxjs';
 
 import { Product } from './product';
 import { ProductCategoryService } from '../product-categories/product-category.service';
@@ -34,7 +34,8 @@ export class ProductService {
       price: product.price? product.price * 1.5 : 0,
       category: categories.find(c=>product.categoryId===c.id)?.name,
       searchKey: [product.productName]
-    })))
+    } as Product))),
+    shareReplay(1)
   )
 
   private productSelectedSubject = new BehaviorSubject<number>(0);
@@ -44,7 +45,8 @@ export class ProductService {
     this.productsWithCategories$, this.productSelectedAction$
   ])
   .pipe(map(([products, selectedProductId])=>
-      products.find(product=> product.id === selectedProductId)))
+      products.find(product=> product.id === selectedProductId)),
+      shareReplay(1))
 
   selectedProductChanged(id: number){
     this.productSelectedSubject.next(id);
